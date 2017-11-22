50a4fd2048da733433d7d78ee0c337324bb34a71
hision <397318057@qq.com>
2017-11-22 18:19:31 +0800

    second commit

---
+++ app/src/main/java/com/he/networksample/FirstUtils.java
@@ -0,0 +1,20 @@
+package com.he.networksample;
+
+/**
+ * Created by Administrator on 2017/11/22.
+ */
+
+public class FirstUtils {
+
+    int age;
+
+    public FirstUtils(int age) {
+        this.age = age;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+
+}
