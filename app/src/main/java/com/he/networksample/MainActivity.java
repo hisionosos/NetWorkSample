aac57e9c8a4114388b2f11215d18e81850195e2e
hision <397318057@qq.com>
2017-11-22 17:48:41 +0800

    first commit

---
+++ app/src/main/java/com/he/networksample/MainActivity.java
@@ -0,0 +1,48 @@
+package com.he.networksample;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import java.util.Vector;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        String str = "sss" + "ddf";
+        str.charAt(0);
+        Vector<String> v = new Vector<>();
+        System.out.println();
+        Vector c= new Vector();
+        System.out.println();
+        for (int i = 0; i < 10; i++) {
+            str += i + "";
+        }
+
+    }
+
+    @Override
+    public void onClick(View view) {
+
+    }
+
+
+}
